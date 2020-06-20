cdcc2498d40998fa2244f0a726fe4ceb7027d96d
elicarcar <elisabetta.carcar@gmail.com>
2020-06-20 15:46:59 +0200

    pages almost done

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PostList = ({ mockPosts }) => {
   return (
@@ -7,15 +8,24 @@ const PostList = ({ mockPosts }) => {
       {mockPosts.map((userPost) => {
         return (
           <div key={userPost.id}>
-            <div>
-              <span>{`Written by ${userPost.user}`}</span>
-              <span>{`Created: ${userPost.post.date}`}</span>
+            <div className="post_info">
+              <span>
+                <strong> Written by</strong>{" "}
+                <Link to={`/user/${userPost.user}`}> {userPost.user}</Link>
+              </span>
+              <span>{`Created at: ${userPost.post.date}`}</span>
             </div>
-            <h6>{userPost.post.title}</h6>
+            <h6>
+              <Link to={`/post/${userPost.id}`}>{userPost.post.title}</Link>
+            </h6>
             <p>{userPost.post.content}</p>
-            {userPost.post.tags.map((tag) => (
-              <span>{tag}</span>
-            ))}
+            <div>
+              {userPost.post.tags.map((tag) => (
+                <span className="post_tags">
+                  <Link to={`/tag/${tag}`}>{`#${tag}`}</Link>
+                </span>
+              ))}
+            </div>
           </div>
         );
       })}
--- client/src/pages/Tag.js
+++ client/src/pages/Tag.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import PostList from "../components/PostList";
 import { discussions, mockPosts } from "../mocks";
@@ -7,26 +7,30 @@ import "../styles/tag.scss";
 
 const Tag = () => {
   const { id } = useParams();
+  const handleFollow = (id) => {
+    console.log(id);
+    console.log("do something using the id");
+  };
+
   return (
     <div className="tag_block">
       <Navbar />
       <div className="inner_block">
         <div className="flex_block">
           <h1>{id}</h1>
-          <button className="btn_follow">Follow Tag</button>
+          <button className="btn_follow" onClick={() => handleFollow(id)}>
+            Follow Tag
+          </button>
         </div>
-        <section className="s_flex_left">
-          <div>
-            <p>Would you like to start a discussion related to this tag?</p>
-            <button>Write a Post</button>
-          </div>
-        </section>
         <section className="s_flex_right">
+          <button>Write a Post</button>
           <div>
             <h6>Discussions on that tag</h6>
             <ul>
-              {discussions.map((discussion) => (
-                <li>{discussion.title}</li>
+              {discussions.map((discussion, i) => (
+                <li>
+                  <Link to={`/post/${discussions.id}`}>{discussion.title}</Link>
+                </li>
               ))}
             </ul>
           </div>
