d0b5c470933794d1af344f7c0b880ff342b66cba
elicarcar <elisabetta.carcar@gmail.com>
2020-06-22 19:09:29 +0200

    private route complete

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    return <Route {...rest} render={props => localStorage.getItem("auth-token")
+        ? (<Component {...props} />)
+        : (<Redirect to={{ pathname="/login", state={ from: props.location } }} />)} />
+};
+
+export default PrivateRoute;
