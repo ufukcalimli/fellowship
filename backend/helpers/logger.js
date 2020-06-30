18304769130e60c6d4824e8ca600581789942c21
ufukcalimli <ufukcalimli>
2020-06-30 16:08:43 +0200

    formatting the log outputs

--- backend/helpers/logger.js
+++ backend/helpers/logger.js
@@ -9,24 +9,30 @@ const logDir = 'log'
 // Create the log directory if it does not exist
 if (!fs.existsSync(logDir)) {
     fs.mkdirSync(logDir);
-  }
+}
 
 const dailyRotateFileTransport = new transports.DailyRotateFile({
     filename: `${logDir}/%DATE%-results.log`,
     datePattern: 'YYYY-MM-DD'
-  });
+});
 
 module.exports = createLogger({
     level: env === 'development'? 'debug' : 'info',
-    format: format.json(),
+    format: format.combine(
+        format.timestamp(),
+        format.label({ label: path.basename(process.mainModule.filename) }),
+        format.json(),
+    ),
     transports: [
         new transports.Console({
             level: 'info',
             format: format.combine(
                 format.colorize(),
-                format.printf( info => `${info.timestamp} ${info.level}: ${info.message}`)
+                format.printf( info => `${Date.now()} ${info.level}: [${info.label}] ${info.message}`)
             )
         }),
         dailyRotateFileTransport
     ]
 })
+
+
