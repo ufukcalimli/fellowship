5b0f8785af60796c54606b2e8621815a53ec04e7
ufukcalimli <ufukcalimli>
2020-11-02 13:00:07 +0100

    changes before commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from "./pages/Landing";
 import Footer from "./components/Footer";
-import NotFoundPage from "./pages/NotFoundPage";
+import NotFound from "./pages/NotFoundPage";
+
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="*" component={NotFoundPage} />
+        <Route exact path="/" component={Landing} />
+        <Route component={NotFound} />
       </Switch>
       <Footer />
     </Router>
